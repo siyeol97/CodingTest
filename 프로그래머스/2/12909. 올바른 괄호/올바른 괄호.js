58e3b7d342fbceef98335c02ef1cf1066e63a484
siyeol <85405709+siyeol97@users.noreply.github.com>
2023-10-14 19:55:29 +0900

    [level 2] Title: 올바른 괄호, Time: 5.67 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,20 @@
+function solution(s){
+    let stack =[];
+    
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === "("){
+            stack.push(s[i]);
+        } else if(s[i] === ")"){
+            if(stack.length === 0){
+                return false;
+            }
+            let temp = stack.pop();
+            if(temp !== "("){
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
+    
+}
