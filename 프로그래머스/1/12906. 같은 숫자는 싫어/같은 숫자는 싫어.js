a17c29dd62934d0a722d6873edf48006cf6ce8b7
siyeol Lee <leesi2830@naver.com>
2024-07-01 20:45:01 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 11.77 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr){
+    const answer = [arr[0]];
+    
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] !== arr[i-1]){
+            answer.push(arr[i])
+        }
+    }
+    
+    return answer;
+}
