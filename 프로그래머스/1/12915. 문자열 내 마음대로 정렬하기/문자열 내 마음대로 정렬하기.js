8262149e0cd371034883faceeb927924efedff9d
siyeol Lee <leesi2830@naver.com>
2024-07-05 13:38:33 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,19 @@
+function solution(strings, n) {
+    strings.sort((a, b) => {
+        if(a[n] < b[n]){
+            return -1;
+        }
+        if(a[n] > b[n]){
+            return 1;
+        }
+        if(a < b){
+            return -1;
+        }
+        if(a > b){
+            return 1;
+        }
+        return 0;
+    })
+    
+    return strings;
+}
